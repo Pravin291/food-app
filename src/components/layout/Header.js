0aeb5796b5f0743aa717a30187baefaa2c45cacc
Pravin Bhosale <pb6884845@gmail.com>
2024-02-02 16:35:56 +0530

    create the home page and register page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,16 +3,26 @@ import Link from "next/link";
 export default function Header() {
   return (
     <>
-    <header className="flex items-center justify-between">
-       <Link className="text-primary font-semibold text-2xl" href="">STREET PIZZA</Link>
-       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-        <Link href={''}>Home</Link>
-        <Link href={''}>Menu</Link>
-        <Link href={''}>About</Link>
-        <Link href={''}>Contact</Link>
-        <Link className="bg-primary text-white px-8 py-2 rounded-full" href={''}>Login</Link>
-       </nav>
-     </header>
+      <header className="flex items-center justify-between">
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+          <Link className="text-primary font-semibold text-2xl" href="/">
+            STREET PIZZA
+          </Link>
+          <Link href={"/"}>Home</Link>
+          <Link href={""}>Menu</Link>
+          <Link href={""}>About</Link>
+          <Link href={""}>Contact</Link>
+        </nav>
+        <nav className="flex items-center gap-4 text-gray-500">
+          <Link href={"/login"}>Login</Link>
+          <Link
+            className="bg-primary text-white px-8 py-2 rounded-full"
+            href={"/register"}
+          >
+            Register
+          </Link>
+        </nav>
+      </header>
     </>
-  )
+  );
 }
